22377c61d161a6b8749078e013e444a3054ac592
Ivan Bachtin <vanobl@mail.ru>
2018-07-21 17:18:53 +0300

    from linux

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
 import sys
 import os
- 
-from PySide2.QtUiTools import QUiLoader
-from PySide2.QtWidgets import QApplication
-from PySide2.QtCore import QFile
+import PySide2
+# from PySide2.QtUiTools import QUiLoader
+# from PySide2.QtWidgets import QApplication
+# from PySide2.QtCore import QFile
 
 from classies.test_form import Form
  
  
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
+    app = PySide2.QtWidgets.QApplication(sys.argv)
     form = Form(os.path.join('mygui', 'main.ui'))
     sys.exit(app.exec_())
